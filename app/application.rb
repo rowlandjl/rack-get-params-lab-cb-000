6623666d1c1d2bbe7a3ba1c499101fdd1d39f559
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-21 16:42:57 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -27,6 +27,9 @@ class Application
       if @@items.include?(add_item)
         @@cart << add_item
         resp.write "added #{add_item}"
+      else 
+        resp.write "We don't have that item"
+      end 
     else
       resp.write "Path Not Found"
     end
