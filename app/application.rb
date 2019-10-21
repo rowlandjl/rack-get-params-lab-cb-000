f8f276ca361152ecd71282ba581505e1f245670e
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-21 16:41:57 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -24,6 +24,8 @@ class Application
       end
     elsif req.path.match(/add/)
       add_item = req.params["item"]
+      if @@items.include?(add_item)
+        
     else
       resp.write "Path Not Found"
     end
